5c378a4f1622b65c07653a04ff507da9d634f6e3
muneneee <lukekevin39@gmail.com>
2023-04-25 02:59:59 +0300

    added store.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+
+    </Provider>
   </React.StrictMode>,
 )
