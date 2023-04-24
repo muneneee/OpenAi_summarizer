444d9e27dac2f75d0fafbfc4a0e3272543768f30
muneneee <lukekevin39@gmail.com>
2023-04-25 01:55:21 +0300

    Set up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+
+import './App.css';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <div className='app'>
+          <Hero />
+          <Demo />
+        </div>
+    </main>
   )
 }
 
