c27c3227090ffa596715871ea486410b7477b1c6
muneneee <lukekevin39@gmail.com>
2023-04-25 02:48:29 +0300

    Created demo component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { logo } from '../assets';
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt='summaz_logo' className='w-28 object-contain' />
 
         <button type='button' onClick={() => window.open('https://github.com/muneneee')} className='black_btn'>
@@ -12,6 +12,14 @@ const Hero = () => {
         </button>
 
       </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient'>OpenAi GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summaz, an open-source article summarizer that transforms lengthy articles inot clear and concise summaries
+      </h2>
     </header>
   )
 }
