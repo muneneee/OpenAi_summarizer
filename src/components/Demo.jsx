5f041a1f06d4ab137af398a11f984eb26f47c745
muneneee <lukekevin39@gmail.com>
2023-04-25 03:38:11 +0300

    intergrated APIs

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-import { copy, linkIcon, loader, tick } from '../assets'
+import { copy, linkIcon, loader, tick } from '../assets';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -8,9 +9,20 @@ const Demo = () => {
     summary: '',
   });
 
-const handleSubmit = async (e) => {
-  alert('Submitted');
-}
+  const [getSummary, { error, isFetching}] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl:article.url });
+
+    if(data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+
+      console.log(newArticle);
+    }
+  }
 
   return (
     <section className='mt-16 w-full max-w-xl'>
