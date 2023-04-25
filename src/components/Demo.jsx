276d15d0d7d48728be6589c89daba2178961229f
muneneee <lukekevin39@gmail.com>
2023-04-25 04:07:44 +0300

    Fetched API data

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,7 +9,19 @@ const Demo = () => {
     summary: '',
   });
 
+  const [allArticles, setAllArticles] = useState([]);
+
   const [getSummary, { error, isFetching}] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    )
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,9 +30,12 @@ const Demo = () => {
 
     if(data?.summary) {
       const newArticle = { ...article, summary: data.summary };
-      setArticle(newArticle);
+      const updatedAllArticles = [newArticle, ...allArticles];
 
-      console.log(newArticle);
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
   }
 
@@ -55,9 +70,57 @@ const Demo = () => {
           </button>
         </form>
         {/* Browse URL History */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className='link_card'
+            >
+              <div className='copy_btn'>
+                <img 
+                  src={copy}
+                  alt='copy_icon'
+                  className='w-[40%] h-[40%] object-contain'
+                />
+
+              </div>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                {item.url}
+              </p>
+
+            </div>
+          ))}
+
+        </div>
 
       </div>
       {/* display results */}
+      <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+              Well, that wasn't supposed to happen
+              <br />
+              <span className='font-satoshi font-normal text-gray-700'>
+                {error?.data?.error}
+              </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+
+      </div>
     </section>
   )
 }
